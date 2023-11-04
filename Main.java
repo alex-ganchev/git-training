837c331a52ddf26a45db598c587400629e7a944c
aganchev <alexander.m.ganchev@gmail.com>
2023-11-04 08:51:47 +0200

    2-bug-fix

--- Main.java
+++ Main.java
@@ -1,7 +1,3 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("---МЕНЮ---");
-        System.out.println("1-Вход");
-        System.out.println("2-Изход");
-    }
+
 }
--- Menu.java
+++ Menu.java
@@ -1,3 +1,7 @@
 public class Menu {
-
+    public static void main(String[] args) {
+        System.out.println("---МЕНЮ---");
+        System.out.println("1-Вход");
+        System.out.println("2-Изход");
+    }
 }
